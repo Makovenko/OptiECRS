67d16ac7b40e22b79478c7e1527f211f65a31133
Mykyta Makovenko <tsumiman@gmail.com>
2020-08-30 00:02:56 -0500

    Initial Commit

---
+++ OptiECRS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OptiECRS
+//
+//  Created by Mykyta Makovenko on 8/30/20.
+//  Copyright © 2020 Texas A&M University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
